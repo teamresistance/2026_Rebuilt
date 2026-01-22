9166294c987b2132b6a019f3508bcd909328706d
darkpotatoo <63860602+darkpotatoo@users.noreply.github.com>
2026-01-22 16:10:26 -0500

    change

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -6,6 +6,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 // Use this file as an example file that allows for referencing positions on a field
 public final class FieldConstants {
 
-  public static Pose2d BLUE_GOAL_CENTER = new Pose2d(4.65, 4.02, Rotation2d.kZero);
-  public static Pose2d RED_GOAL_CENTER = new Pose2d(-1, -1, Rotation2d.kZero); // TODO: this
+  public static final Pose2d BLUE_GOAL_CENTER = new Pose2d(4.65, 4.02, Rotation2d.kZero);
+  public static final Pose2d RED_GOAL_CENTER = new Pose2d(-1, -1, Rotation2d.kZero); // TODO: this
 }
