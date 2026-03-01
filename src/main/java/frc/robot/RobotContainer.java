e9445375feea1bde3c92430336543b22783154fa
darkpotatoo <63860602+darkpotatoo@users.noreply.github.com>
2026-02-28 21:50:57 -0500

    get rid of center

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -53,7 +53,6 @@ public class RobotContainer {
   public final PhotonCamera frontRightCamera = new PhotonCamera("front-right");
   public final PhotonCamera backLeftCamera = new PhotonCamera("back_left");
   public final PhotonCamera backRightCamera = new PhotonCamera("back_right");
-  public final PhotonCamera frontCenterCamera = new PhotonCamera("front-center");
   private final Alert cameraFailureAlert;
 
   // Subsystems
@@ -175,12 +174,7 @@ public class RobotContainer {
   private VisionSubsystem configureAprilTagVision() {
     try {
       vision =
-          new VisionSubsystem(
-              frontLeftCamera,
-              frontRightCamera,
-              backRightCamera,
-              frontCenterCamera,
-              backLeftCamera);
+          new VisionSubsystem(frontLeftCamera, frontRightCamera, backRightCamera, backLeftCamera);
       vision.setDataInterfaces(drive::getPose, drive::addAutoVisionMeasurement);
 
     } catch (IOException e) {
